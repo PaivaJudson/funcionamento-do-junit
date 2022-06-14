0bae0a3002418e2890321c0a014ea3cfcbe53f36
judsonpaiva <judson.paiva@hotmail.com>
2022-06-14 08:57:46 +0100

    Testes em cascatas

--- src/test/java/com/example/junit/EntendendoJUnitTest.java
+++ src/test/java/com/example/junit/EntendendoJUnitTest.java
@@ -1,2 +1,36 @@
-package com.example.junit;public class EntendendoJUnit {
+package com.example.junit;
+
+import org.junit.*;
+
+public class EntendendoJUnitTest {
+
+    @BeforeClass
+    public static void iniciarTeste(){
+        System.out.println("Método de teste que Será executado antes de todos os outros métodos");
+    }
+
+    @AfterClass
+    public static void terminarTeste(){
+        System.out.println("Será executado depois de todos os outros métodos de teste");
+    }
+
+    @Test
+    public void testandoInstrucoes(){
+        System.out.println(">>>testandoInstrucoes()<<<");
+    }
+
+    @Before
+    public void metodoDepoisDoBefore(){
+        System.out.println("Executar antes de cada método de teste excepto do BeforeClass");
+    }
+
+    @After
+    public void metodoAntesdoAfter(){
+        System.out.println("Executar depois de cada método de teste excepto do AfterClass");
+    }
+
+    @Test
+    public void outroTeste(){
+        System.out.println("Testando outra coisa");
+    }
 }
